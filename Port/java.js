f5bbab30dab32abf965708fc47123a722398ef6d
omarabdelaziz404 <110203238+omarabdelaziz404@users.noreply.github.com>
2024-10-22 17:37:22 +0300

    Add files via upload

---
+++ Port/java.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    alert("Button clicked!");
+}
+function goToContact() {
+    window.location.href = "./ass/contact.html"; 
+}
+function goToAbout() {
+    window.location.href = "./ass/about.html";
+}
+function goToPrevious () {
+    window.location.href = "./ass/previous projects.html";
+}
+function goToScreen (){
+    window.location.href = "./ass/Photos.html";
+}
